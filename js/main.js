3c736d0f1c0033a16104ba29eeba965547e81af5
Drew Bent <drew3x@gmail.com>
2020-05-05 12:00:43 -0700

    Add filters to footer.

--- js/main.js
+++ js/main.js
@@ -94,6 +94,28 @@ $(document).ready(function () {
       "language": {
         "zeroRecords": noResultsStr
       },
+      "initComplete": function () {
+        this.api().columns().every(function(i) {
+          if (i==1 || i==3 || i==4) {
+            var column = this;
+            var select = $('<select  class="browser-default custom-select form-control-sm"><option value="" selected>All</option></select>')
+                .appendTo( $(column.footer()).empty() )
+                .on( 'change', function () {
+                    var val = $.fn.dataTable.util.escapeRegex(
+                        $(this).val()
+                    );
+
+                    column
+                      .search( val ? '^'+val+'$' : '', true, false )
+                      .draw();
+                } );
+
+            column.data().unique().sort().each( function ( d, j ) {
+                select.append( '<option value="'+d+'">'+d+'</option>' )
+            } );
+          }          
+        });
+      }
     });
     $('.dataTables_length').addClass('bs-select');
   });
